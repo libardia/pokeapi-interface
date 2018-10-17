66714870d01a7fcd1e23ffb19f73208a25722366
Anthony Libardi <libardi42@gmail.com>
2018-10-17 08:41:59 -0500

    Testing resolve()

--- src/main/java/info/tonyl/pokeapi/PokeApiInterfaceApp.java
+++ src/main/java/info/tonyl/pokeapi/PokeApiInterfaceApp.java
@@ -1,7 +1,11 @@
 package info.tonyl.pokeapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import info.tonyl.pokeapi.core.PokeApiInterface;
 import info.tonyl.pokeapi.core.impl.PokeApiCallerSpring;
+import info.tonyl.pokeapi.models.ApiResource;
 import info.tonyl.pokeapi.models.Berry;
 import info.tonyl.pokeapi.models.BerryFirmness;
 
@@ -10,9 +14,17 @@ public class PokeApiInterfaceApp {
 		PokeApiInterface api = new PokeApiInterface(new PokeApiCallerSpring());
 
 		Berry b = api.getBerry("cheri");
-		BerryFirmness firmness = (BerryFirmness) b.getFirmness().resolve(api, BerryFirmness.class);
+		BerryFirmness firmness = b.getFirmness().resolve(api, BerryFirmness.class);
 
 		System.out.println(b.toString());
 		System.out.println(firmness.toString());
+
+		List<Berry> berries = new ArrayList<>();
+		for (ApiResource br : firmness.getBerries()) {
+			berries.add(br.resolve(api, Berry.class));
+		}
+
+		System.out.println("All " + firmness.getName() + " berries:");
+		System.out.println(berries.toString());
 	}
 }
